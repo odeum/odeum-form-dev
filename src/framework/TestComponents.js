e75a575aef8cb5ddbd10bc7a2cf26028dc415ba0
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2017-12-20 00:09:54 +0100

    Playing with Page for login purposes. Doesn't work wothput a contitional rendering wrapper

--- src/framework/TestComponents.js
+++ src/framework/TestComponents.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
 import { Menu } from 'odeum-app'
 import FormNew from '../content/Form/FormNew'
+import { Redirect } from 'react-router-dom'
 
 export class Page extends Component {
 	render() {
-		return (
-			<Menu {...this.props} />
-		)
+		const { protect } = this.props
+		if (protect) {
+			console.log(protect)
+			return (
+				<Redirect to={'/login'} />
+				// <Menu route={'/login'} {...this.props}>
+				// 	{this.props.children}
+				// </Menu>
+			) 
+		}
+		else 
+			return ( 
+				<Menu {...this.props} >
+					{this.props.children}
+				</Menu>
+			)		 
 	}
 }
 
 export class Login extends Component {
 	render() {
 		return (
-			<div>
-				{this.props.form}
-				{this.props.action}
+			<div label={'Login'}>
 				{this.props.children}
 			</div>
 		)
