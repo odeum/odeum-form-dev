cda533800c28ba42437221d0bf1134a4cbac1934
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2017-12-20 02:42:21 +0100

    Added StyledTextArea for styling TextArea inputs

--- src/content/Form/FormComponent/FormStyles.js
+++ src/content/Form/FormComponent/FormStyles.js
@@ -26,7 +26,6 @@ export const Input = styled.input`
 export const StyledInput = Input.extend`
 	height: 10%; 
 	width: 100%;
-	/* margin-top: 15px;  */
 	margin-bottom: 15px;
 	color: ${(props) => props.color ? props.color : '#13A085'};
 `
@@ -56,7 +55,35 @@ export const Select = styled.select`
 export const StyledSelect = Select.extend`
 	height: 40px; 
 	width: 100%;
-	/* margin-top: 15px;  */
+	margin-bottom: 15px;
+	color: ${(props) => props.color ? props.color : '#13A085'};
+`
+
+export const TextArea = styled.textarea`    
+	font-size: 16px;
+	font-weight: 200;
+    padding: 1rem;
+    color: ${(props) => props.color ? props.color : '#2C3E50'};
+    background: #ECF0F1;
+    border: none;
+	border-radius: 4px;
+    box-sizing: border-box;
+	outline: none;
+
+    &:hover {		
+        border-color: ${transparentize(0.7, '#3B97D3')};		 
+		box-shadow: ${`0 0 0 3px ${transparentize(0.7, '#3B97D3')}`};
+		cursor: ${(props) => props.isDisabled ? 'not-allowed' : 'pointer'};
+    }
+
+    &:focus {
+		box-shadow: ${(props) => props.focusColor ? `0 0 0 3px ` + props.focusColor : `0 0 0 3px #13A085`};
+    }
+`
+
+export const StyledTextArea = TextArea.extend`
+	height: 80px; 
+	width: 100%;
 	margin-bottom: 15px;
 	color: ${(props) => props.color ? props.color : '#13A085'};
 `
