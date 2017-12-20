b048ecd88a5a648b07b7bb93b985bf1899860b5f
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2017-12-20 02:59:55 +0100

    Refactored to stateless component and deleted props

--- src/content/Form/FormComponent/Field.js
+++ src/content/Form/FormComponent/Field.js
@@ -1,35 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { StyledInput } from './FormStyles'
 
-// Called by Common Field Component
-class Field extends Component {
-	render() {
-		const fieldId = 'email'
-		return (
-			<div>
-				<StyledInput
-					type={this.props.type}
-					// name={fieldId}
-					// value={this.props.value}
-					innerRef={this.props.createInputRef ? this.props.createInputRef(fieldId) : null}
-					onChange={this.props.handleChange}
-					// validate={this.props.validate}
-					// color={!this.props.isValid ? '#BE4F44' : undefined}
-					// focusColor={!this.props.isValid ? '#BE4F44' : undefined}
-					onFocus={this.props.handleFocus}
-					autoComplete={this.props.autoComplete}
-					onBlur={this.props.handleBlur}
-					onMouseEnter={this.props.handleMouseEnter}
-					onMouseLeave={this.props.handleMouseLeave}
-					required={this.props.required}
-					disabled={this.props.disabled}
-					readOnly={this.props.readOnly}
-					{...this.props}
-				/>
-			</div>
-		)
-	}
+const Field = (props) => {	
+	return (
+		<StyledInput
+			type={props.type}
+			innerRef={props.createInputRef ? props.createInputRef(props.name) : null}
+			onChange={props.handleChange}
+			{...props}
+		/>
+	)
 }
 
 export default Field
-
